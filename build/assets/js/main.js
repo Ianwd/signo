e4d3ace38a2939e49470f648166bd8b88eba4d3e
Mathieu Delvaux <mathieudelvaux.567@hotmail.com>
2017-05-01 05:04:05 +0200

    :art: Switched Gulp to ES6

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,3 +1,3 @@
 "use strict";!function(e,t,n,c,s,a,o){e.GoogleAnalyticsObject=s,e[s]=e[s]||function(){(e[s].q=e[s].q||[]).push(arguments)},e[s].l=1*new Date,a=t.createElement(n),o=t.getElementsByTagName(n)[0],a.async=1,a.src="https://www.google-analytics.com/analytics.js",o.parentNode.insertBefore(a,o)}(window,document,"script",0,"ga");
-"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}var _modernizr=require("modernizr"),_modernizr2=_interopRequireDefault(_modernizr);
+"use strict";
 //# sourceMappingURL=main.js.map
